09cf4c7d336dddecdad6153a07729df146a4bf66
??? <marvin.sincini@studenti.unicam.it>
2025-03-24 22:50:03 +0100

    PUSH

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # CopyClickFx
  
 Progetto per avere piccole schermate da cliccare per copiarsi codici e password, sempre a portata di mano!
+
+Quando avvii il jar, il testo inserito come argomenti verrà copiato al click del bottone.
+Se si vuole customizzare la scritta, si può usare l'argomento --label o -l
+Es. java -jar app.jar testo da copiare --label click me!
--- src/main/java/fx/App.java
+++ src/main/java/fx/App.java
@@ -2,7 +2,6 @@ package fx;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
 import javafx.scene.input.Clipboard;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.layout.VBox;
@@ -11,33 +10,63 @@ import javafx.stage.Stage;
 public class App extends Application {
     @Override
     public void start(Stage primaryStage) {
-        TextField textField = new TextField();
-        textField.setPromptText("Inserisci del testo");
 
-        Button copyButton = new Button("Copia");
-
+        Button copyButton = new Button(getLabel());
+        copyButton.setPrefSize(200, 200);
+        copyButton.setStyle(
+            "-fx-font-size: 25px;" +
+            "-fx-text-fill: white;" +
+            "-fx-background-color: black;"
+        );
+        
+        copyButton.setOnMousePressed(e -> copyButton.setStyle("-fx-background-color: #222222; -fx-text-fill: white; -fx-font-size: 25px;"));
+        copyButton.setOnMouseReleased(e -> copyButton.setStyle("-fx-background-color: black; -fx-text-fill: white; -fx-font-size: 25px;"));
         copyButton.setOnAction(event -> {
-            String textToCopy = textField.getText();
-
-            if (!textToCopy.isEmpty()) {
+            if (args.length > 0) {
                 Clipboard clipboard = Clipboard.getSystemClipboard();
                 ClipboardContent content = new ClipboardContent();
-                content.putString(textToCopy);
+                content.putString(getValue());
                 clipboard.setContent(content);
             }
         });
 
-        VBox vbox = new VBox(10, textField, copyButton);
-        
-        Scene scene = new Scene(vbox, 300, 200);
-
-        primaryStage.setTitle("Copy Button Example");
+        VBox vbox = new VBox(0, copyButton);
+        Scene scene = new Scene(vbox, 200, 200);
         primaryStage.setScene(scene);
         primaryStage.show();
-
     }
 
+    private String getLabel(){
+        String s = "";
+        boolean get = false;
+        boolean first = false;
+        for(int i = 0; i < args.length; i++){
+            if(args[i].equals("--label") || args[i].equals("-l")){
+                get = true;
+                first = true;
+                continue;
+            }
+            if(!get) continue;
+            s += (first ? "" : " ") + args[i];
+            first = false;
+        }
+        return s.equals("") ? "Copy" : s;
+    }
+
+    private String getValue(){
+        String s = "";
+        for(int i = 0; i < args.length; i++){
+            if(args[i].equals("--label") || args[i].equals("-l"))
+                break;
+            s += (i == 0 ? "" : " ") + args[i];
+        }
+        return s;
+    }
+
+    public static String[] args;
+
     public static void run(String[] args) {
+        App.args = args;
         launch();
     }
 }
